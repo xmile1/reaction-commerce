327295612ec2913c057b1b6df1b715d920b6a27c
Uyiosa Enabulele <hiskonxeptz@gmail.com>
2017-01-09 22:36:45 +0100

    refactor(tour): add tour template in html to improve readability [WIP #137005181]

--- imports/plugins/included/tour/client/tour.js
+++ imports/plugins/included/tour/client/tour.js
@@ -1,7 +1,12 @@
 import Shepherd from "tether-shepherd";
 import "/node_modules/tether-shepherd/dist/css/shepherd-theme-arrows.css";
 import { Accounts } from "/lib/collections";
-import * as template from "./template";
+import { Template } from 'meteor/templating'
+import "./tour.html";
+
+
+
+
 
 function options (whoseTour) {
   return {defaults: {
@@ -27,9 +32,6 @@ function getButton(whichTour){
     var actionBack = ()=>{
       vendorTour.back();
     }
-    var actionBack = ()=>{
-      vendorTour.hide();
-    }
   }
 
   else if (whichTour == "buyerTour") {
@@ -51,7 +53,7 @@ const allButtons = {
     classes: 'shepherd-button-secondary',
     action: () => {
       Accounts.update({_id: Meteor.userId()}, {$set: {takenTour: true}});
-      return actionNext
+      return actionHide()
     }
   },
   next:
@@ -65,56 +67,83 @@ const allButtons = {
    text: 'Back',
    action: actionBack,
    classes: 'shepherd-button-example-primary'
- }
+ },
+ finish:
+ {
+  text: 'Finish',
+  action: actionHide,
+  classes: 'shepherd-button-example-primary'
+}
 
 }
 return allButtons;
 };
 
-vendorTour.addStep("Welcome", {
-  title: "Welcome to Reaction Commerce",
-  text: template.welcomeText,
-  buttons: [getButton("vendorTour").nexttime, getButton("vendorTour").next]
-});
 
-vendorTour.addStep("toDashboard", {
-  title: "customize Store",
-  text: template.aboutDashboard,
-  attachTo: ".tour-accounts bottom",
-  advanceOn: "#dropown-apps-dashboard click"
+let template={};
+Template.tour.onRendered(function () {
+  template.welcomeText = this.$('.welcomeText').html();
+  template.aboutDashboard = this.$('.aboutDashboard').html();
+  template.dashboardDetails = this.$('.dashboardDetails').html();
+  template.product = this.$('.product').html();
+  template.profileOrdersAccounts = this.$('.profileOrdersAccounts').html();
+  template.shop = this.$('.shop').html();
+  template.retakeTour = this.$('.retakeTour').html();
 
-});
 
-vendorTour.addStep("dashboard", {
-  title: "customize Store",
-  text: template.dashboardDetails,
-  advanceOn: "#dropown-apps-dashboard click"
-});
+  vendorTour.addStep("Welcome", {
+    title: "Welcome to Reaction Commerce",
+    text: template.welcomeText,
+    buttons: [getButton("vendorTour").nexttime, getButton("vendorTour").next]
+  });
 
-vendorTour.addStep("newProduct", {
-  title: "Create New Product",
-  text: template.product,
-  attachTo: ".tour-create-content left",
-  advanceOn: ".docs-link clink"
-});
+  vendorTour.addStep("toDashboard", {
+    title: "customize Store",
+    text: template.aboutDashboard,
+    attachTo: ".tour-accounts bottom",
+    advanceOn: "#dropown-apps-dashboard click"
 
-vendorTour.addStep("profileOrdersAccounts", {
-  title: "Orders",
-  text: template.profileOrdersAccounts,
-  advanceOn: ".docs-link clink"
-});
+  });
 
-vendorTour.addStep("visitShop", {
-  title: "Shop",
-  text: template.shop,
-  attachTo: ".rui.tag.link bottom",
-  advanceOn: ".tour-shop",
-  buttons: [getButton("vendorTour").nextTime,getButton("vendorTour").back, getButton("vendorTour").next]
-});
+  vendorTour.addStep("dashboard", {
+    title: "customize Store",
+    text: template.dashboardDetails,
+    advanceOn: "#dropown-apps-dashboard click"
+  });
 
-vendorTour.addStep("toViewnextTime", {
-  title: "Information",
-  text: template.retakeTour,
-  // attachTo: ".user-accounts right",
-  advanceOn: ".docs-link clink"
+  vendorTour.addStep("newProduct", {
+    title: "Create New Product",
+    text: template.product,
+    attachTo: ".tour-create-content left",
+    advanceOn: ".docs-link clink"
+  });
+
+  vendorTour.addStep("profileOrdersAccounts", {
+    title: "Orders",
+    text: template.profileOrdersAccounts,
+    advanceOn: ".docs-link clink"
+  });
+
+  vendorTour.addStep("visitShop", {
+    title: "Shop",
+    text: template.shop,
+    attachTo: ".rui.tag.link bottom",
+    advanceOn: ".tour-shop",
+    buttons: [getButton("vendorTour").nexttime, getButton("vendorTour").back, getButton("vendorTour").finish ]
+  });
+
+  vendorTour.addStep("toViewnextTime", {
+    title: "Information",
+    text: template.retakeTour,
+    // attachTo: ".user-accounts right",
+    advanceOn: ".docs-link clink"
+  });
+
+  buyerTour.addStep("shop", {
+    title: "Shopping",
+    text: template.retakeTour,
+    // attachTo: ".user-accounts right",
+    advanceOn: ".docs-link clink"
+  });
+
 });
