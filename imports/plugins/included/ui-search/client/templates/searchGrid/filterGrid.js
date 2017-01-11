6a2cd93a2d33ee5167c6ad33316f7bd54126cc53
enaho murphy <murphy.enaho@andela.com>
2017-01-11 07:33:43 +0100

    Add product sort by price to search.

--- imports/plugins/included/ui-search/client/templates/searchGrid/filterGrid.js
+++ imports/plugins/included/ui-search/client/templates/searchGrid/filterGrid.js
@@ -1,24 +1,20 @@
-import { Tags, Products } from "/lib/collections";
+import { Session } from "meteor/session";
 
 Template.filterGrid.helpers({
-  displayTags: function () {
-    const tags = Tags.find().fetch();
-    return tags;
-  },
-
-  displayPrice: function () {
-    const priceRange = ["$10 - $20", "$30 - $50", "$60 -$70" ];
-    return priceRange;
-  },
-
-  displayBrand: function () {
-    let vendors = [];
-    const products = Products.find().fetch();
-    products.forEach
-  },
-  displaySellers: function () {
-
+  getBrands(products) {
+    return _.uniq(_.pluck(products, "vendor"));
   }
 });
 
+Template.filterGrid.events({
+  "change #price-filter": function (event) {
+    Session.set("priceFilter", event.target.value);
+  },
+  "change #sort-value": function (event) {
+    Session.set("sortValue", event.target.value);
+  },
+  "change #brand-filter": function (event) {
+    Session.set("brandFilter", event.target.value);
+  }
+});
 
