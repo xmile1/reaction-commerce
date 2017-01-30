b328d6dfac1eb1c1f99f6cbab90946d8682a188a
andela-msosan <moyosore.sosan@andela.com>
2017-01-30 13:56:09 +0100

    test(social): write test for review method
    Finishes #137005197

---
+++ server/methods/review.app-test.js
@@ -0,0 +1,35 @@
+/* eslint dot-notation: 0 */
+/* eslint-disable no-undef */
+import { expect } from "meteor/practicalmeteor:chai";
+import { sinon} from "meteor/practicalmeteor:sinon";
+import Fixtures from "/server/imports/fixtures";
+import { Reaction } from "/server/api";
+import { Reviews } from "/lib/collections";
+
+Fixtures();
+
+describe("Core review methods", function () {
+  let sandbox;
+
+  beforeEach(function () {
+    sandbox = sinon.sandbox.create();
+  });
+
+  afterEach(function () {
+    sandbox.restore();
+  });
+
+  describe("Add review to products", function () {
+    beforeEach(function () {
+      Reviews.remove({});
+    });
+
+    it("should create review for product", function () {
+      sandbox.stub(Reaction, "hasOwnerAccess", () => true);
+      const review = {productId: 23456, rating: 2, comment: "This is a comment", username: "moyosore"};
+      Meteor.call("review/insert", review);
+      const newReview = Reviews.find({productId: 23456}).count();
+      expect(newReview).to.equal(1);
+    });
+  });
+});
