7a2115fffebe5818faafc30cec0752b02be55f97
Raphael Akpan <raphael.akpan@andela.com>
2017-02-06 15:11:19 +0100

    feature(analytics): Use bar charts to display

--- imports/plugins/included/actionable-analytics/client/templates/analyticsHighestSelling.js
+++ imports/plugins/included/actionable-analytics/client/templates/analyticsHighestSelling.js
@@ -9,7 +9,6 @@ let productDetails = [];
 let productCounts = [];
 let productIds = [];
 let productNames = [];
-const backgroundColor = ["#FF6384", "#36A2EB", "#FFCE56", "#FE7EA6", "#2ACEEB"];
 
 const storeProductData = (productId) => {
   if (productData[productId]) {
@@ -30,14 +29,37 @@ const getProducts = (productId) => {
 const displayProducts = () => {
   const ctx = document.getElementById("sellingProductChart");
   const myChart = new Chart(ctx, {
-    type: "doughnut",
+    type: "bar",
     data: {
       labels: productNames,
       datasets: [{
+        label: "Products Sold",
         data: productCounts,
-        backgroundColor: backgroundColor,
-        hoverBackgroundColor: backgroundColor
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)"
+        ],
+        borderColor: [
+          "rgba(255,99,132,1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)"
+        ],
+        borderWidth: 1
       }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      }
     }
   });
 };
--- imports/plugins/included/actionable-analytics/client/templates/analyticsMostPatronisedBrand.js
+++ imports/plugins/included/actionable-analytics/client/templates/analyticsMostPatronisedBrand.js
@@ -9,7 +9,6 @@ let vendourDetails = [];
 let vendourCounts = [];
 let vendourIds = [];
 let vendourNames = [];
-const backgroundColor = ["#FF6384", "#36A2EB", "#FFCE56", "#FE7EA6", "#2ACEEB"];
 
 const storeProductData = (productId) => {
   if (productData[productId]) {
@@ -36,14 +35,37 @@ const getVendours = (vendourId) => {
 const displayVendours = () => {
   const ctx = document.getElementById("pieChart");
   const myChart = new Chart(ctx, {
-    type: "pie",
+    type: "bar",
     data: {
       labels: vendourNames,
       datasets: [{
+        label: "Products Sold",
         data: vendourCounts,
-        backgroundColor: backgroundColor,
-        hoverBackgroundColor: backgroundColor
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)"
+        ],
+        borderColor: [
+          "rgba(255,99,132,1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)"
+        ],
+        borderWidth: 1
       }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      }
     }
   });
 };
